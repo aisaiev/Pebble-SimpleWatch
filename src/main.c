e77a931e8d202f509a6e4ab76515320dac07047c
detores <antonio.de.tores@gmail.com>
2015-09-13 11:59:50 +0300

    Add weather update every 1 hour

--- src/main.c
+++ src/main.c
@@ -103,10 +103,23 @@ void main_window_unload(Window *window) {
   fonts_unload_custom_font(WeekFont);
   fonts_unload_custom_font(DateFont);
   fonts_unload_custom_font(WeatherFont);
- }
+}
   
 void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   update_time();
+  
+  // Get weather update every 1 hour
+  if(tick_time->tm_hour % 1 == 0) {
+    // Begin dictionary
+    DictionaryIterator *iter;
+    app_message_outbox_begin(&iter);
+
+    // Add a key-value pair
+    dict_write_uint8(iter, 0, 0);
+
+    // Send the message!
+    app_message_outbox_send();
+  }
 }
 
 static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
