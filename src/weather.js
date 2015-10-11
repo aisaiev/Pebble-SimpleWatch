dcb8ce20e3dc53931b914c09d9a69e3ea048094e
Anton Isaev <antonio.de.tores@gmail.com>
2015-10-11 14:15:42 +0300

    Add OpenWeather API key

--- src/weather.js
+++ src/weather.js
@@ -10,7 +10,7 @@ var xhrRequest = function (url, type, callback) {
 function locationSuccess(pos) {
   // Construct URL
   var url = "http://api.openweathermap.org/data/2.5/weather?lat=" +
-      pos.coords.latitude + "&lon=" + pos.coords.longitude;
+      pos.coords.latitude + "&lon=" + pos.coords.longitude + "&APPID=a41d1d46e2d9075acbe27655af273216";
 
   // Send request to OpenWeatherMap
   xhrRequest(url, 'GET', 
